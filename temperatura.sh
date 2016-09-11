f1f99fd705bc102198f63f7195918029b965c7a0
orangepipl <biuro@orangepi.pl>
2016-09-11 14:02:56 +0000

    Odczyt temperatury

--- temperatura.sh
+++ temperatura.sh
@@ -1,1 +1,11 @@
-#!/bin/bash
+#! /bin/bash
+
+# pobranie ustawień
+source ./config
+
+# pobranie temperatury
+TMP=$(cat $TERMOMETR | grep  -E -o ".{0,0}t=.{0,5}" | /usr/bin/cut -c 3-)
+
+# wyswietlenie temperatury
+echo "$TMP 1000" | awk '{printf "%.2f\n", $1/$2}'
+
